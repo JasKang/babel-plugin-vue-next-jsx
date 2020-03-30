e15514959b4d3328788c6022b31877a074fbf540
jaskang <kangchaomin@dayuwuxian.com>
2020-03-31 00:19:28 +0800

    feat:children jsx

--- test/index.test.ts
+++ test/index.test.ts
@@ -6,16 +6,39 @@ const plugins = [BabelPluginVueNextJsx];
 const parseCode = (example: string) =>
   transformSync(example, { plugins })!.code;
 
-it('parses the jsx', () => {
+it('base', () => {
   const example = `const A = () => {} 
-const Comp = () => <A sx={{ mb: '3rem', lineHeight: 4 }} />`;
+const Comp = () => <A style={{ height: '3rem', lineHeight: 4 }} />`;
   const code = parseCode(example);
   expect(code).toMatch(`const A = () => {};
 
 const Comp = () => h(A, {
-  "sx": {
-    mb: '3rem',
+  "style": {
+    height: '3rem',
     lineHeight: 4
   }
 }, []);`);
+});
+
+it('children', () => {
+  const example = `const A = () => {} 
+const Comp = () => (
+<A style={{ height: '3rem', lineHeight: 4 }}>
+  <div>test</div>
+  <div style={{height:'4px'}}>test</div>
+</A>
+)`;
+  const code = parseCode(example);
+  expect(code).toMatch(`const A = () => {};
+
+const Comp = () => h(A, {
+  "style": {
+    height: '3rem',
+    lineHeight: 4
+  }
+}, [h("div", {}, ["test"]), h("div", {
+  "style": {
+    height: '4px'
+  }
+}, ["test"])]);`);
 });
