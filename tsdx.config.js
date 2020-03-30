41cb505a35a37e7f729c5748b266c9367e22f6bc
jaskang <kangchaomin@dayuwuxian.com>
2020-03-31 00:25:04 +0800

    chore: target node

--- tsdx.config.js
+++ tsdx.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   // This function will run for each entry/format/env combination
   rollup(config, options) {
-    config.target = 'node';
+    // config.target = 'node';
     return config; // always return a config.
   },
 };
